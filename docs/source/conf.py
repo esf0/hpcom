b74525351f19d5dc358a43a553b414e61a3c9833
esf0 <egor.sedoff@gmail.com>
2023-04-13 16:32:11 +0100

    Fixing the readthedocs
    {minicooper} Fixing the readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,10 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../hpcom'))
-sys.path.insert(0, os.path.abspath('../tests'))
+sys.path.insert(0, os.path.abspath('../../..'))
+sys.path.insert(0, os.path.abspath('../../hpcom'))
+sys.path.insert(0, os.path.abspath('../../tests'))
 
 # -- Project information -----------------------------------------------------
 
